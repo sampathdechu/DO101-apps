0a7dbac2b9b7bc33fb1f5035dacfc03e535f7625
sampathdechu <sampath@Sampaths-MacBook-Pro.local>
2020-06-07 17:22:35 +0530

    version1

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('This is the version 1 of the app!\n');
 });
 
 app.listen(8080, function () {
